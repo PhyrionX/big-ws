d2bf37afd45ca6b29f231170138b810c5826fc9a
phyrionX <rgcmb@hotmail.com>
2015-10-26 15:49:36 +0100

    Probando el api translate yandex

--- src/test/java/translator/service/TranslatorServiceTest.java
+++ src/test/java/translator/service/TranslatorServiceTest.java
@@ -20,8 +20,8 @@ public class TranslatorServiceTest {
     TranslatorService translatorService;
     @Test
     public void translateTest() throws Exception {
-        TranslatedText translatedText = translatorService.translate("en", "es", "This is a test of translation service");
-        assertEquals("Esto es una prueba de servicio de",translatedText.getTranslation());
+        TranslatedText translatedText = translatorService.translate("en", "es", "I love the subject of web engineering");
+        assertEquals("Me encanta la asignatura de ingeniería web",translatedText.getTranslation());
     }
 
 }
